b37ca5e67fd8289d60539a049e30b98288710ba8
Yongyao Jiang <J.yongyao11@gmail.com>
2017-03-09 20:41:11 -0500

    Initial implementation

---
+++ core/src/main/java/esiptestbed/mudrod/streaming/LogStreaming.java
@@ -0,0 +1,79 @@
+package esiptestbed.mudrod.streaming;
+
+import java.io.Serializable;
+import java.util.Comparator;
+import java.util.List;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.streaming.Duration;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+
+import scala.Tuple2;
+
+public class LogStreaming {
+  private static final Function2<Long, Long, Long> SUM_REDUCER = (a, b) -> a + b;
+  
+  private static final Function2<Session, Session, Session> Session_Merger = 
+      new Function2<Session, Session, Session>() {
+    @Override public Session call(Session s1, Session s2) {
+      return s1.add(s2);
+    }
+  };
+  
+  public static void main(String[] args) throws InterruptedException {
+    SparkConf conf = new SparkConf().setAppName("MUDROD Streaming");
+    JavaSparkContext sc = new JavaSparkContext(conf);
+
+    JavaStreamingContext jssc = new JavaStreamingContext(sc,
+        new Duration(3000));  // This sets the update window to be every 3 seconds.
+    
+    jssc.checkpoint("checkpoints-mudrod-streaming-total");
+     
+    //tail -f [[YOUR_LOG_FILE]] | nc -lk 9999
+    //nc -lk 9999
+    //monitor folder
+//    if(args.length==0){
+//      System.out.println("Please input the log file path!");
+//      return;
+//    }
+//    JavaDStream<String> logDataDStream = jssc.textFileStream(args[0]);
+    JavaReceiverInputDStream<String> logDataDStream =
+        jssc.socketTextStream("localhost", 9999);
+
+    // A DStream of Apache Access Logs.
+    JavaDStream<ApacheAccessLog> accessLogDStream =
+        logDataDStream.map(ApacheAccessLog::parseFromLogLine)
+        .filter(log->log.isCrawler()==false);
+    
+    JavaPairDStream<Integer, Long> responseCodeCountDStream = accessLogDStream
+        .mapToPair(s -> new Tuple2<>(s.getResponseCode(), 1L))
+        .reduceByKey(SUM_REDUCER);
+    responseCodeCountDStream.foreachRDD(rdd -> {
+      System.out.println("Response code counts: " + rdd.take(100));
+    });
+    
+    // A DStream of ipAddresses accessed > 10 times.
+    JavaPairDStream<String, Session> ipDStream = accessLogDStream
+        .mapToPair(s -> new Tuple2<>(s.getIpAddress(), new Session(s)))
+        .reduceByKey(Session_Merger);
+    
+    ipDStream.foreachRDD(rdd -> {
+      List<Tuple2<String, Session>> sessions = rdd.take(100);
+      for(Tuple2<String, Session> t:sessions)
+      {
+        System.out.println(t._1 + " " + t._2.getLogList().size());
+      }
+    });
+    
+    // Start the streaming server.
+    jssc.start();              // Start the computation
+    jssc.awaitTermination();   // Wait for the computation to terminate
+  }
+}
+
---
+++ core/src/main/java/esiptestbed/mudrod/streaming/Session.java
@@ -0,0 +1,99 @@
+package esiptestbed.mudrod.streaming;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+
+public class Session implements Serializable{
+
+  public Session() {
+    // TODO Auto-generated constructor stub
+  }
+
+  private String start_time;
+  private String end_time;
+  private String ip;
+  private List<ApacheAccessLog> logList = new ArrayList<ApacheAccessLog>();
+
+  //private DateTimeFormatter formatter = DateTimeFormat.forPattern("dd/MMM/yyyy:HH:mm:ss Z");
+
+  public Session(ApacheAccessLog log)
+  {
+    this.start_time = log.getDateTimeString();
+    this.end_time = log.getDateTimeString();
+    this.ip = log.getIpAddress();
+    this.logList.add(log);
+  }
+
+  public Session(String start_time, String end_time, String ip, List<ApacheAccessLog> list)
+  {
+    this.start_time = start_time;
+    this.end_time = end_time;
+    this.ip = ip;
+    this.logList = list;
+  }
+
+  public String getStartTime()
+  {
+    return start_time;
+  }
+
+  public String getEndTime()
+  {
+    return end_time;
+  }
+
+  public String getIpAddress()
+  {
+    return ip;
+  }
+  
+  public List<ApacheAccessLog> getLogList()
+  {
+    return logList;
+  }
+
+  public void setStartTime(String time)
+  {
+    start_time = time;
+  }
+
+  public void setEndTime(String time)
+  {
+    end_time = time;
+  }
+
+  public void setIpAddress(String ipString)
+  {
+    ip = ipString;
+  }
+
+  public Session add(Session s)
+  {
+    DateTimeFormatter formatter = DateTimeFormat.forPattern("dd/MMM/yyyy:HH:mm:ss Z");
+    DateTime new_start_time = formatter.parseDateTime(s.getStartTime()); 
+    DateTime new_end_time = formatter.parseDateTime(s.getEndTime());
+    List<ApacheAccessLog> new_logList = new ArrayList<ApacheAccessLog>();
+    
+    if(new_start_time.isAfter(formatter.parseDateTime(this.getStartTime())))
+    {
+      new_start_time = formatter.parseDateTime(this.getStartTime());
+    }
+    
+    if(new_end_time.isBefore(formatter.parseDateTime(this.getEndTime())))
+    {
+      new_end_time = formatter.parseDateTime(this.getEndTime());
+    }
+    
+    new_logList.addAll(s.getLogList());
+    new_logList.addAll(this.getLogList());
+    
+    return new Session(new_start_time.toString(formatter), new_end_time.toString(formatter), this.getIpAddress(), new_logList);
+  }
+
+}
